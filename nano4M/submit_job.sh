b9266c983ebd1c1f80cff80712450821ad22bfd8
Roman Bachmann <roman.bachmann@epfl.ch>
2025-03-25 13:57:28 +0000

    nano4M - Part 1

---
+++ nano4M/submit_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=you_job_name         # Change as needed
+#SBATCH --time=02:00:00
+#SBATCH --account=com-304
+#SBATCH --qos=com-304
+#SBATCH --gres=gpu:2                    # Request 2 GPUs
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4               # Adjust CPU allocation if needed
+#SBATCH --output=interactive_job.out    # Output log file
+#SBATCH --error=interactive_job.err     # Error log file
+
+CONFIG_FILE=$1
+WANDB=$2
+NUM_GPUS=$3
+
+conda activate nanofm
+export WANDB_API_KEY=$WANDB $$ OMP_NUM_THREADS=1 torchrun --nproc_per_node=$NUM_GPUS run_training.py --config $CONFIG_FILE
