ab1b697f2c438afb7baa3fc5b31442fd07c9764c
Sacha Godey <sacha.godey@epfl/ch>
2025-03-26 18:17:56 +0100

    change device type

--- nano4M/nanofm/models/gpt.py
+++ nano4M/nanofm/models/gpt.py
@@ -126,7 +126,7 @@ class GPT(nn.Module):
         # Hint: What shape should the mask have such that each token can attend to itself and
         # all previous tokens, but not to any future tokens?
         mask = torch.tril(torch.ones(L, L)).unsqueeze(0).bool()
-        mask = mask.to(self.device())
+        mask = mask.to(x.device)
             
         # TODO: Forward pass through Transformer trunk
         # Hint: Make sure to pass the causal mask to the transformer trunk too
